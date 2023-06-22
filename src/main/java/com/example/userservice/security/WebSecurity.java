0d783d20b580c6493e14f23e9f7a6bd0ae769bff
hsshin <hyunseo99999@naver.com>
2023-06-23 08:48:21 +0900

    User security 수정

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,76 +1,56 @@
 package com.example.userservice.security;
 
 import com.example.userservice.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authorization.AuthorizationDecision;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
-import org.springframework.security.web.util.matcher.IpAddressMatcher;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
 
     private UserService userService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Autowired
     public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
-    AuthenticationManager authenticationManager;
-
-    // spring.boot 2.7 부터는 WebSecurityConfigurerAdapter가 아닌
-    // SecurityFilterChain 을 사용 합니다.
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-        authenticationManager = authenticationManagerBuilder.build();
-
-        //AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-        //authenticationFilter.setAuthenticationManager(authenticationManager);
-
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager , userService);
-
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/**")
+//                .hasIpAddress("127.0.0.1")
+//                .and()
+//                .addFilter(getAuthenticationFilter());
 
         http.authorizeRequests()
-                //.antMatchers("/error/**").permitAll() // public abstract java.lang.String javax.servlet.ServletRequest.getRemoteAddr() is not supported 보기 싫을때 활성화
-                .mvcMatchers("/")
-                .access(hasIpAddress("127.0.0.1").toString())
-                .and()
-                .authenticationManager(authenticationManager)
-                .addFilter(authenticationFilter)
-        ;
-        http.headers().frameOptions().disable();
+                .antMatchers("/actuator/**").permitAll();
 
-        return http.build();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .addFilter(getAuthenticationFilter());
+        http.headers().frameOptions().disable();
     }
 
-    private static AuthorizationManager<RequestAuthorizationContext> hasIpAddress(String ipAddress) {
-        IpAddressMatcher ipAddressMatcher = new IpAddressMatcher(ipAddress);
-        return (authentication, context) -> {
-            HttpServletRequest request = context.getRequest();
-            return new AuthorizationDecision(ipAddressMatcher.matches(request));
-        };
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager(), userService);
+//        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -5,12 +5,13 @@ import com.example.userservice.dto.UserDto;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-
     UserDto createUser(UserDto userDto);
 
     UserDto getUserByUserId(String userId);
 
     Iterable<UserEntity> getUserByAll();
+
+    UserDto getUserDetailsByEmail(String userName);
 }
 
 
